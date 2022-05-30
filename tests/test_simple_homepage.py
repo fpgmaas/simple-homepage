5f1d8e3a8920b4f35fbe3d7c3fa190e20d67f30a
Florian Maas <florian.maas@ingka.ikea.com>
2022-05-30 08:39:44 +0200

    removed testing prints

--- tests/test_simple_homepage.py
+++ tests/test_simple_homepage.py
@@ -20,17 +20,6 @@ def file_contains_text(file: str, text: str) -> bool:
 
 def test_init_command(tmp_path):
     with run_within_dir(tmp_path):
-        print(f"cwd: {os.getcwd()}")
-        print(
-            f"files in /home/runner/work/simple-homepage/simple-homepage/simple_homepage/: {os.listdir('/home/runner/work/simple-homepage/simple-homepage/simple_homepage/')}"
-        )
-        print(
-            f"files in /home/runner/work/simple-homepage/simple-homepage/: {os.listdir('/home/runner/work/simple-homepage/simple-homepage/')}"
-        )
-        print(f"files in /home/runner/work/simple-homepage/: {os.listdir('/home/runner/work/simple-homepage')}")
-        print(
-            f"files in /home/runner/work/simple-homepage/simple-homepage/.venv: {os.listdir('/home/runner/work/simple-homepage/simple-homepage/.venv')}"
-        )
 
         subprocess.check_call(shlex.split("homepage init")) == 0
         expected_files = [
